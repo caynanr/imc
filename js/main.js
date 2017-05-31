c334f397dab9924dd926bbe6fecfe8bcdb3f7242
caynanr <caynanramos@gmail.com>
2017-05-31 20:37:47 -0300

    atualização no code do main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 function imc()
 {
-  var p1 = document.getElementById('peso1').value;
-  var a2 = document.getElementById('altura1').value;
+  var peso = document.getElementById('peso1').value;
+  var altura = document.getElementById('altura1').value;
 
-  var q2 = (a2*a2);
-  var soma = (p1/q2);
-  var soma2 = soma.toFixed(1);
-  document.getElementById('resultado1').value = ('IMC='+ soma2);
+  var imc = peso / (altura*altura);
+  document.getElementById('resultado1').value = ('IMC='+ imc.toFixed(2));
 }
