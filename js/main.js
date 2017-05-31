f4a318bd58faac667fab2afa28d9b86c16c65026
caynanr <caynanramos@gmail.com>
2017-05-31 20:45:08 -0300

    correções

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ function imc()
 {
   var peso = document.getElementById('peso1').value;
   var altura = document.getElementById('altura1').value;
-
   var imc = peso / (altura*altura);
   document.getElementById('resultado1').value = ('IMC='+ imc.toFixed(2));
 }
